cbe71eb7d64a77f7a8369eb7f80d29947466e85f
GEORGE_G15\heroo <george_kreye@du.edu>
2023-04-04 15:38:46 -0600

    Add BPLib C++ class

---
+++ Kreye_GP2/Source/Kreye_GP2/Labs/BPLib.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPLib.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KREYE_GP2_API UBPLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
