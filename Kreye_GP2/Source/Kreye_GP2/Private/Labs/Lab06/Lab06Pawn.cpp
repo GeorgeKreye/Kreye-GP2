cba93109cbefacb0d97194d170c3353c9a27f69a
George Kreye <george.kreye@du.edu>
2023-04-26 23:00:10 -0600

    Start Lab 6

---
+++ Kreye_GP2/Source/Kreye_GP2/Private/Labs/Lab06/Lab06Pawn.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Labs/Lab06/Lab06Pawn.h"
+
+// Sets default values
+ALab06Pawn::ALab06Pawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create mesh component
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	// Get default pawn mesh
+	// auto CapsuleMesh = ConstructorHelpers::FObjectFinder<UStaticMesh>(
+	// 	TEXT("/Game/StarterContent/Shapes/Shape_NarrowCapsule.Shape_NarrowCapsule"));
+	// if (CapsuleMesh.Succeeded())
+	// {
+	// 	Mesh->SetStaticMesh(CapsuleMesh.Object);
+	// 	Mesh->SetRelativeLocation(FVector(0.0,0.0,0.0));
+	// }
+	//
+	// // Get default pawn material
+	// auto PawnMaterial = ConstructorHelpers::FObjectFinder<UMaterialInterface>(
+	// 	TEXT("/Game/StarterContent/Materials/M_Basic_Floor.M_Basic_Floor"));
+	// if(PawnMaterial.Succeeded())
+	// {
+	// 	Mesh->SetMaterial(0,PawnMaterial.Object);
+	// }
+}
+
+// Called when the game starts or when spawned
+void ALab06Pawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALab06Pawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ALab06Pawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
