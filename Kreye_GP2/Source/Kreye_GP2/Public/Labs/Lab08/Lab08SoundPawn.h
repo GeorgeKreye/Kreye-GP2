e296b18f806b06c429f097a9d2ec7e7f418f2c67
George Kreye <george.kreye@du.edu>
2023-05-04 01:59:30 -0600

    Finish Lab 8

--- Kreye_GP2/Source/Kreye_GP2/Public/Labs/Lab08/Lab08SoundPawn.h
+++ Kreye_GP2/Source/Kreye_GP2/Public/Labs/Lab08/Lab08SoundPawn.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputMappingContext.h"
 #include "GameFramework/Pawn.h"
 #include "Lab08SoundPawn.generated.h"
 
@@ -16,22 +17,40 @@ public:
 	ALab08SoundPawn();
 
 	// The mesh used by this pawn
-	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = Mesh)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Mesh)
 	TObjectPtr<UStaticMeshComponent> Mesh;
 
 	// The audio component used to play sound
-	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = Sound)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sound)
 	TObjectPtr<UAudioComponent> Sound;
+
+	// The audio component used to play background music
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sound)
+	TObjectPtr<UAudioComponent> Music;
+
+	// The input action used for playing sound
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> PlaySoundAction;
+
+	// The input mapping context used for triggering sound playing action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TSoftObjectPtr<UInputMappingContext> InputMap;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// Called when the spacebar is pressed; tells audio component to play sound
+	virtual void SoundTriggered(const struct FInputActionInstance& Instance);
+
+private:
+	// Determines whether to play a sound this tick
+	bool PlaySoundThisTick;
 };
